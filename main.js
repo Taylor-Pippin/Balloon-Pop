614d67f3714c8aa19d84ff64b2c745949728f575
Taylor-Pippin <taylor.a.pippin98@gmail.com>
2021-03-15 15:59:35 -0600

    Balloon Changes

--- main.js
+++ main.js
@@ -7,40 +7,71 @@ let width = 100
 let inflationRate = 20
 let maxsize = 300
 let popCount = 0
+let gameLength = 5000
+let clockID = 0
+let timeRemaining = 0
 
 function startGame(){
-
   startButton.setAttribute("disabled", "true")
   inflateButton.removeAttribute("disabled")
+  startClock()
 
-  setTimeout(() => {
-    console.log("it's been three seconds")
-
-    inflateButton.setAttribute("disabled", "true")
-    startButton.removeAttribute("disabled")
-    
-
-  }, 3000)
+  setTimeout(stopGame, gameLength)
 }
+
+function startClock(){
+  timeRemaining = gameLength
+  clockID = setInterval(drawClock, 1000)
+}
+
+function stopClock(){
+  clearInterval(clockID)
+}
+
+function drawClock(){
+  let countdownElem = document.getElementById('countdown')
+  timeRemaining -= 1000
+  countdownElem.innerText = (timeRemaining / 1000).toString()
+}
+
 
 function inflate() {
   clickCount++
-  let balloonElement = document.getElementById("balloon")
   height += inflationRate
   width += inflationRate
-
+  
   if(height >= maxsize){
     console.log("pop the balloon")
     popCount++
     height = 0;
     width = 0;
-    document.getElementById('pop-count').innerText = popCount.toString()
   }
+  draw()
+}
 
+function draw(){
+  let balloonElement = document.getElementById("balloon")
+  let clickCountElem = document.getElementById("click-count")
+  let popCountElem = document.getElementById('pop-count')
+  
   balloonElement.style.height = height + "px"
   balloonElement.style.width = width + "px"
-
-
-  let clickCountElem = document.getElementById("click-count")
+  
   clickCountElem.innerText = clickCount.toString()
+  popCountElem.innerText = popCount.toString()
+}
+
+function stopGame(){
+  console.log("The game is over")
+
+  inflateButton.setAttribute("disabled", "true")
+  startButton.removeAttribute("disabled")
+
+  clickCount = 0
+  height = 120
+  width = 100
+
+  stopGame()
+
+  draw()
 }
